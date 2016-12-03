53b1e52af8de308256f8bbca19a08fad4038fbcc
gcbest <godfrey.c.best@gmail.com>
2016-12-03 15:19:24 -0500

    twitter keys updated

--- app/config/keys.js
+++ app/config/keys.js
@@ -1,12 +1,16 @@
-exports.twitterKeys = {
-    consumer_key: '3YQoTNAeLXqedFytaYsm7XAqv',
-    consumer_secret: 'lXebEZx5rv6ToeLeXHNX43PTsCYgQZnhBxeRwNmtC9FaQgExc5',
-    access_token_key: '799057205119123456-PE3tV1pYtOEyDpUdV8i3P3UjjBJ0OTN',
-    access_token_secret: '03RvoJ1rHN7AmgiE6rsecJ4DXVCxIqTfywDmu6fSdbuH9'
-}
+module.exports = {
 
-exports.passportKeys = {
-    consumerKey: '3YQoTNAeLXqedFytaYsm7XAqv',
-    consumerSecret: 'lXebEZx5rv6ToeLeXHNX43PTsCYgQZnhBxeRwNmtC9FaQgExc5',
-    callbackURL: 'http://litterbot.herokuapp.com/login/twitter/return'
+    twitterKeys :{
+        consumer_key: '3YQoTNAeLXqedFytaYsm7XAqv',
+        consumer_secret: 'lXebEZx5rv6ToeLeXHNX43PTsCYgQZnhBxeRwNmtC9FaQgExc5',
+        access_token_key: '799057205119123456-PE3tV1pYtOEyDpUdV8i3P3UjjBJ0OTN',
+        access_token_secret: '03RvoJ1rHN7AmgiE6rsecJ4DXVCxIqTfywDmu6fSdbuH9'
+    },
+
+    passportKeys : {
+        consumerKey: '3YQoTNAeLXqedFytaYsm7XAqv',
+        consumerSecret: 'lXebEZx5rv6ToeLeXHNX43PTsCYgQZnhBxeRwNmtC9FaQgExc5',
+        callbackURL: 'http://litterbot.herokuapp.com/login/twitter/return'
+    }
+
 }
